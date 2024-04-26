652e58ebc0040ddb218627b69fb23d859606055d
Karl Catigbe <karl.catigbe@cbsinteractive.com>
2024-04-26 10:40:40 -0400

    add report

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,44 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { Octokit } from '@octokit/action';
 
 async function run(): Promise<void> {
   try {
-    const testReports = core.getMultilineInput('test-reports');
-    const coverageReports = core.getMultilineInput('coverage-reports');
+    if (core.getInput('token')) {
+      const testReports = core.getMultilineInput('test-reports');
+      const coverageReports = core.getMultilineInput('coverage-reports');
 
-    testReports.forEach(testReport => {
-      core.info(`Parsing file ${testReport}`);
-    });
+      testReports.forEach(testReport => {
+        core.info(`Parsing file ${testReport}`);
+      });
 
-    coverageReports.forEach(coverageReport => {
-      core.info(`Parsing file ${coverageReport}`);
-    });
+      coverageReports.forEach(coverageReport => {
+        core.info(`Parsing file ${coverageReport}`);
+      });
+
+      core.summary.addRaw("Summary").write();
+
+      let octokit = new Octokit();
+
+      const pr = github.context.payload.pull_request
+      const sha = (pr && pr.head.sha) || github.context.sha
+      
+      await octokit.checks.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        name: 'Jest Reports',
+        head_sha: sha,
+        status: 'completed',
+        conclusion: 'success',
+        output: {
+          title: "Hi",
+          summary: "Report summary",
+          text: "This is text"
+        }
+
+      });
+
+    }
   } catch (error) {
     core.setFailed((error as Error).message);
   }
